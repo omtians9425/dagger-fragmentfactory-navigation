12ff0de6e663a9467b5458ea894f014bbc92dccd
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-17 22:12:54 +0900

    create SecondFragment

---
+++ app/src/main/java/com/omtians9425/dagger_fragmentfactory_navigation/SecondFragment.kt
@@ -0,0 +1,32 @@
+package com.omtians9425.dagger_fragmentfactory_navigation
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class SecondFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = SecondFragment()
+    }
+
+    private lateinit var viewModel: SecondViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.second_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(SecondViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/omtians9425/dagger_fragmentfactory_navigation/SecondViewModel.kt
@@ -0,0 +1,7 @@
+package com.omtians9425.dagger_fragmentfactory_navigation
+
+import androidx.lifecycle.ViewModel
+
+class SecondViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
