fe68b0de61c0292974abf67ce381720a9a05b1d3
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-17 22:28:52 +0900

    add ViewModelfactory to object graph

--- app/src/main/java/com/omtians9425/dagger_fragmentfactory_navigation/di/ViewModelModule.kt
+++ app/src/main/java/com/omtians9425/dagger_fragmentfactory_navigation/di/ViewModelModule.kt
@@ -1,14 +1,19 @@
 package com.omtians9425.dagger_fragmentfactory_navigation.di
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.omtians9425.dagger_fragmentfactory_navigation.FirstViewModel
 import com.omtians9425.dagger_fragmentfactory_navigation.SecondViewModel
+import com.omtians9425.dagger_fragmentfactory_navigation.ui.ViewModelFactory
 import dagger.Binds
 import dagger.Module
 import dagger.multibindings.IntoMap
 
 @Module
 interface ViewModelModule {
+
+    @Binds
+    fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
 
     @Binds
     @IntoMap
