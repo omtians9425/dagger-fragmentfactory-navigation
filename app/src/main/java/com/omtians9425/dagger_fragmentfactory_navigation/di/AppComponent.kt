b230e41e59d190a2da67ccc78f3488fb635d0e38
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-17 22:35:59 +0900

    add modules to AppComponent

--- app/src/main/java/com/omtians9425/dagger_fragmentfactory_navigation/di/AppComponent.kt
+++ app/src/main/java/com/omtians9425/dagger_fragmentfactory_navigation/di/AppComponent.kt
@@ -6,7 +6,7 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [])
+@Component(modules = [ViewModelModule::class, FragmentModule::class])
 interface AppComponent {
 
     @Component.Builder
